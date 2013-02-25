f2db4901e26fa90bf69d6948365e1cbe67c05dfc
suttonj <jssutton11@gmail.com>
2013-02-24 21:27:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Festivals::Application.config.secret_token = '5c600441bed97e91834d0cb151bc774d3dca141dcb16245b6bf714549eca95ff3ecfd41eff684d83670cd6f439a097c032c829508801a0712820b8efe88e7e3a'
